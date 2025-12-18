9a7069ad1084561c0e197a3fd088377b289bcfb2
bala-amypo <bala@amypo.in>
2025-12-18 05:04:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/studentEntity.java
+++ src/main/java/com/example/demo/entity/studentEntity.java
@@ -10,5 +10,21 @@ import jakarata.validation.constraints.*;
      @NotBlank(message = "Name is not Valid")
      private String name;
 
-     @Email(message ="")
+     @Email(message ="Email Format is not Valid")
+     private string email;
+     
+     public void setId(Long id){
+        this.id=id;
+     }
+     public Long getId(){
+        return id;
+     }
+     public studentEntity(){
+
+     }
+     public stundentEntity(Long id ,String name,String email){
+        this .id=id;
+        this.name=name;
+        this.email=email;
+     }
  }
